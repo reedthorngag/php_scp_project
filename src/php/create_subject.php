3f18c23ab729c2a53c432cdae7999c929e0c6866
Brody Cooper <reed.thorngag@gmail.com>
2023-10-17 14:55:13 +1300

    .

--- src/php/create_subject.php
+++ src/php/create_subject.php
@@ -15,12 +15,10 @@ require "utils/db.php";
 
 if (insert('subjects',$_POST,'sssss','subject','class','image','description','containment_info')) {
     echo 'Success!';
-    $conn->close();
     die(0);
 }
 
 http_response_code(422);
 echo "You fucked up lol";
-$conn->close();
 
 ?>
--- src/php/fetch_subject.php
+++ src/php/fetch_subject.php
@@ -4,12 +4,9 @@ include "errors.php";
 $result = select('subjects',['*'],$_GET,'s','subject');
 if (!$result) {
     http_response_code(404);
-    $conn->close();
     die(0);
 }
 
 echo json_encode($result);
-
-$conn->close();
 
 ?>
