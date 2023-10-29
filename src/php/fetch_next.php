6e74a7526eace3bef3c71e44dc510d043853a82f
Brody Cooper <reed.thorngag@gmail.com>
2023-10-30 12:12:39 +1300

    .

--- src/php/fetch_next.php
+++ src/php/fetch_next.php
@@ -1,12 +1,13 @@
 <?php
 include "errors.php";
 
-require 'db.php';
-
+require 'utils/utils.php';
 if (!check_set($_GET,'skip')) {
     http_response_code(422);
     die(0);
 }
+
+require 'db.php';
 
 $query = $db->conn->prepare('SELECT * FROM subjects LIMIT 10 SKIP ?');
 $query->bind_param('i',$_GET['skip']);
